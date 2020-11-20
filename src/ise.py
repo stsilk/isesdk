860a6783a30172c8c544dd1ccb37f4549e405cce
Steven Silk <ssilk@rmcweb.com>
2020-11-20 12:40:52 -0500

    Added verify connection Function

--- src/ise.py
+++ src/ise.py
@@ -1,4 +1,6 @@
 import ipaddress
+import subprocess
+import platform
 
 #Class to handle cisco ISE session
 class ISE:
@@ -6,11 +8,27 @@ class ISE:
     __ip = ''
     __username = ''
     __password = ''
+    __baseURL = ''
 
     def __init__(self, username, password, ip):
-        self.__username = username
-        self.__password = password
+        self.username = username
+        self.password = password
         self.ip = ip
+        self.clientSystem = platform.system()
+
+    def verifyConnection(self):
+        print("Pinging ISE Server...")
+        if self.clientSystem == 'Windows':
+            params = '-n 1 -w 5'
+        else:
+            params = '-c 1' 
+        command = ['ping', params, self.ip]  
+        try:
+            subprocess.check_output(command)
+            print("Successfully pinged server @ {0}".format(self.ip))
+        except Exception as e:
+            print("Failed to ping server @ {0}".format(self.ip))
+            return 
 
     @property
     def username(self):
@@ -30,7 +48,7 @@ class ISE:
     
     @password.setter
     def password(self, value):
-        self.__password = self.value
+        self.__password = value
 
     @ip.setter
     def ip(self, value):
